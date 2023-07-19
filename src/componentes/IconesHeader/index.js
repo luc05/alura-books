efa77973c58ec469634d8dbbbb8406a3f30f5172
lucas silva <lucassilva.ferre@gmail.com>
2023-07-18 21:00:56 -0300

    Alterando CSS para Styled Component

--- src/componentes/IconesHeader/index.js
+++ src/componentes/IconesHeader/index.js
@@ -1,20 +1,28 @@
-import "./estilo.css"
 import perfil from "../../imagens/perfil.svg"
 import sacola from "../../imagens/sacola.svg"
+import styled from "styled-components"
 const icones = [perfil, sacola]
+const IconesContainer = styled.ul`
+  display: flex;
+  align-items: center;
+`
+const Icone = styled.li`
+  margin-right: 40px;
+  width: 25px;
+`
 
 function IconesHeader () {
     return (
-        <ul className='icones'>
+        <IconesContainer>
         { icones.map ( (icone) => (
           <li className='icone'>
-            <img 
+            <Icone 
             src={icone} 
             alt='icone'
-            ></img>
+            />
           </li>
         ))}
-    </ul>
+    </IconesContainer>
     )
 }
 export default IconesHeader
