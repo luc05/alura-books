876b81ecde938eee533fed56d4a2f80291bde9c3
lucas silva <lucassilva.ferre@gmail.com>
2023-07-19 21:13:36 -0300

    Criando a barra de pesquisa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './componentes/Header';
 import styled from 'styled-components'
+import Pesquisa from './componentes/Pesquisa';
 
 const AppContainer = styled.div`
 width: 100vw;
@@ -10,6 +11,7 @@ function App() {
   return (
     <AppContainer>
       <Header/>
+      <Pesquisa/>
     </AppContainer>
   );
 }
---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,7 @@
+import livro1 from "../../imagens/livro.png"
+import livro2 from "../../imagens/livro2.png"
+
+export const livros = [
+    {nome: "Curso Angular", id: 1, src:livro1},
+    {nome: "Projeto .Net", id: 2, src:livro2}
+]
